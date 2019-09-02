f938a840361bd106ef5be8095ba5d1d8e271bf5b
Masha Reko <Masha.Reko@holidaycheck.com>
2019-09-02 10:41:45 +0200

    ''

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,13 @@
 const assert = require('assert');
 
 const generateBoard = (height, width) => {
-    if (height === 1 && width === 1)
-    return [[0]];
-
-    return [[0, 0], [0, 0]];
+    var board =  new Array(height);
+    for(var i = 0; i < board.length; i++)
+    {
+        var temp = new Array(width);
+        board[i] = temp.fill(0);
+    }
+    return board;
 }
 
 describe('gamOfLife', () => {
