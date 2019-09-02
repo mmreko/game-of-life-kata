ab2fe4bc4c2ea0feacfb64044cee02cbc5e30def
Masha Reko <Masha.Reko@holidaycheck.com>
2019-09-02 10:30:26 +0200

    ''

--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,13 @@
 const assert = require('assert');
 
+const boardGenerator = (height, width) => {
+    return [[0]];
+}
 
+describe('gamOfLife', () => {
+    describe('boardGenerator', () => {
+        it('returns a board with the given dimensions', () => {
+            assert.deepEqual(boardGenerator(1, 1), [[0]]);
+        })
+    })
+})
